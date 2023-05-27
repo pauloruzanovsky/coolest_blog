6d92777b55df63a20749e0468e8cc4bb5ae6f422
Paulo Ruzanovsky <pauloruzanovsky@gmail.com>
2023-05-27 17:09:40 -0300

    clearing up main

--- config/database.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config({ path: './config/config.env' });
-import { MongoClient } from 'mongodb';
-
-const url = process.env.MONGODB_URI;
-const client = new MongoClient(url);
-client.connect();
-export const db = client.db('twitter');
